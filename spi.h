1c692db87f32969df18f853594a7dbe6817eae90
Jesenko Mehmedbasic <jesenko@mehmedbasic.dk>
2018-03-13 22:32:36 +0100

    Removed unused stuff from spi library.

--- spi.h
+++ spi.h
@@ -1,22 +1,4 @@
 /*
- * author: Robert/Duality
- * git: github.com/duality4y
- * 
- * source inspired by:
- * http://www.brianhensley.net/2012/07/getting-spi-working-on-raspberry-pi.html
- * 
- * also verry usefull little document:
- * http://lxr.free-electrons.com/source/include/uapi/linux/spi/spidev.h#L52
- * 
- * this source is for communicating over the spi. that the raspberrypi offers.
- * 
- * 
- * a rewrite to make it more like a "library"
- * for easier use.
- * although by now it is heavely modified.
- * 
- * spi.h
- *
  * Copyright (C) 2014 Duality
  *      Robert van der Tuuk <201292@live.nl>
  *
@@ -30,8 +12,11 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
+ * author: Robert/Duality
+ * git: github.com/duality4y
  *
- * 
+ * Unused methods and constants removed to better fit the use case of
+ * the hidjoy2spi project.
  * */
 
 #ifndef SPI_H
@@ -47,35 +32,25 @@
 #include <linux/types.h>
 #include <linux/spi/spidev.h>
 
-#define ARRAY_SIZE(a) (sizeof(a) / sizeof((a)[0]))
-//default spi buffer transmist/receive size.
-#ifndef SPI_BUFFER_SIZE
-	#define SPI_BUFFER_SIZE 20
-#endif
+#define SPI_BUFFER_SIZE 20
 
-//for debugging purposes.
-//#define DEBUG
-
-//static const char *spi_device = "/dev/spidev0.0";
 extern uint8_t spi_mode;
 extern uint8_t spi_bits;
 extern uint32_t spi_speed;
-extern uint16_t spi_delay;
 
 void pabort(const char *s);
-int transfer(int, uint8_t *, uint8_t *);
-int spi_set_mode(int, int);
-int spi_set_word(int, int);
-int spi_set_speed(int, int);
-void spi_set_delay(int);
-int spiOpen(const char*);
-int spiClose(int);
-void spi_init(int);
 
-#ifdef DEBUG
-void loopbackTest(int);
-void printSpiDetails();
-#endif
+int transfer(int, uint8_t *, uint8_t *);
+
+int spi_set_mode(int, uint8_t);
+
+int spi_set_word(int, uint8_t);
+
+int spi_set_speed(int, uint32_t);
+
+void spi_set_delay(int);
+
+void spi_init(int);
 
 #endif
 
