1ee3e330176e8d3658c287b701e2ff4258afdd23
Jesenko Mehmedbasic <jesenko@mehmedbasic.dk>
2018-03-16 21:54:01 +0100

    Better button handling

--- Xpad.c
+++ Xpad.c
@@ -7,14 +7,18 @@
 
 uint8_t sourceType = SRC_XBOX_360;
 
-long map(long x, long in_min, long in_max, long out_min, long out_max) {
-    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+uint8_t map(signed short x) {
+    long in_min = -32767;
+    long in_max = 32767;
+    long out_max = 255;
+    return (uint8_t) ((x - in_min) * (out_max) / (in_max - in_min));
 }
 
 void handle360(XpadReport_Data_t *dst, struct js_event *event) {
     switch (event->type) {
         case JS_EVENT_BUTTON: {
             uint8_t button = (uint8_t) (event->value ? 0xFF : 0);
+            uint8_t buttonDigital = (uint8_t) (event->value ? 1 : 0);
             switch (event->number) {
                 case 0:
                     dst->BUTTON_A = button;
@@ -34,6 +38,21 @@ void handle360(XpadReport_Data_t *dst, struct js_event *event) {
                 case 5:
                     dst->BUTTON_WHITE = button;
                     break;
+                case 6:
+                    dst->BUTTON_BACK = buttonDigital;
+                    break;
+                case 7:
+                    dst->BUTTON_START = buttonDigital;
+                    break;
+                case 8:
+                    dst->UNUSED_1 = buttonDigital;
+                    break;
+                case 9:
+                    dst->BUTTON_L3 = buttonDigital;
+                    break;
+                case 10:
+                    dst->BUTTON_R3 = buttonDigital;
+                    break;
                 default:
                     break;
             }
@@ -48,7 +67,7 @@ void handle360(XpadReport_Data_t *dst, struct js_event *event) {
                     dst->LEFT_STICK_X = event->value;
                     break;
                 case 2:
-                    dst->TRIGGER_LEFT = (uint8_t) (map(event->value, -32767, 32767, 0, 255) & 0xFF);
+                    dst->TRIGGER_LEFT = map(event->value);
                     break;
                 case 3:
                     dst->RIGHT_STICK_Y = event->value;
@@ -57,7 +76,7 @@ void handle360(XpadReport_Data_t *dst, struct js_event *event) {
                     dst->RIGHT_STICK_X = event->value;
                     break;
                 case 5:
-                    dst->TRIGGER_RIGHT = (uint8_t) (map(event->value, -32767, 32767, 0, 255) & 0xFF);
+                    dst->TRIGGER_RIGHT = map(event->value);
                     break;
                 case 6:
                     dst->DPAD_LEFT = (uint8_t) (event->value == -32767 ? 1 : 0);
